5299bd39b28be7ad0731f23d1aa504cc29d343c9
hsan256 <47532941+hsan256@users.noreply.github.com>
2021-04-10 19:56:38 +0100

    cree repository

--- src/Controller/RegimeController.php
+++ src/Controller/RegimeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Regime;
+use App\Repository\RegimeRepository;
 use Doctrine\ORM\Repository\RepositoryFactory;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,10 +14,8 @@ class RegimeController extends AbstractController
     /**
      * @Route("/regimes", name="regimes_index")
      */
-    public function index(): Response
+    public function index(RegimeRepository $repo): Response
     {
-        $repo = $this->getDoctrine()->getRepository(Regime::class);
-
         $regimes = $repo->findAll();
 
         return $this->render('admin/regime/index.html.twig', [
--- src/Entity/Regime.php
+++ src/Entity/Regime.php
@@ -76,9 +76,23 @@ class Regime
         return $this->type;
     }
 
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
     public function getDescription(): ?string
     {
         return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
     }
 
     public function getImage(): ?string
@@ -86,4 +100,10 @@ class Regime
         return $this->image;
     }
 
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
 }
