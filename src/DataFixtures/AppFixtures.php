eb6b00d0496353816e6b0273e2cd58420af535cc
hsan256 <47532941+hsan256@users.noreply.github.com>
2021-04-16 00:53:55 +0100

    add fixture to entity menu

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use Faker\Factory;
+use App\Entity\Menu;
 use App\Entity\Regime;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -22,6 +23,23 @@ class AppFixtures extends Fixture
             $regime->setType($title)
             ->setDescription($description)
             ->setImage("regime.jpg");
+
+            for($j = 1; $j <= mt_rand(2,5) ; $j++){
+                $menu = new Menu();
+
+                $menu->setIdRegime($regime)
+                    ->setDescription($faker->sentence(1))
+                    ->setNumJour(mt_rand(1,30))
+                    ->setMatin($faker->sentence(1))
+                    ->setMatinImg(mt_rand(1,5).".jpg")
+                    ->setDejeuner($faker->sentence(1))
+                    ->setDejeunerImg(mt_rand(1,5).".jpg")
+                    ->setDinner($faker->sentence(1))
+                    ->setDinnerImg(mt_rand(1,5).".jpg")
+                    ->setTotalCalories(mt_rand(1500,3000))
+                ;
+                $manager->persist($menu);
+            }
 
             $manager->persist($regime);
         }
